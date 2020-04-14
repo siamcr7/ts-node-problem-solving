9a0236ce4c9a0360646bd940e6e286a0cefc3fcd
Jamil <siam_cr7@yahoo.com>
2020-04-14 20:26:41 +0600

    Optimized input processing method to remove TLE

--- src/class/RunProgram.ts
+++ src/class/RunProgram.ts
@@ -12,54 +12,36 @@ export class RunProgram {
     process.stdin.setEncoding('utf8');
     process.stdin.on('data', (c) => (this.inputString += c));
     process.stdin.on('end', () => {
-      // const { EOL } = require('os');
-      // const lines = this.inputString.split(EOL); /*your input text, split by lines*/
-      //   // console.log(lines);
-      //   solution(lines.filter(line => line.trim().length > 0));
-      // const newSolution = new Solution(
-      //   this.convertSpaceSepartedValues(lines.filter((line) => line.trim().length > 0)).map((line) => +line)
-      // );
-      const newSolution = new Solution(this.convertSpaceSepartedValues());
+
+      const newSolution = new Solution(this.processInputString());
     });
   }
 
-  private convertSpaceSepartedValues(): number[] {
-    // let result: string[] = [];
-
-    // lines.forEach(line => {
-    //   // result = [...result, ...line.split(' ')];
-
-    // });
-    // const { EOL } = require('os');
-    const result: number[] = [];
-
-    // console.log('SEE INP: ',  this.inputString);
+  /**
+   * Optimized to reduce TLE while taking input
+   */
+  private processInputString<T>(): T[] {
+    const result: T[] = [];
 
     let madeUp = '';
     for (let i = 0; i < this.inputString.length; i++) {
       const currentChar = this.inputString.charAt(i);
+
+      // if empty or other character is found
       if (currentChar === ' ' || currentChar.length !== currentChar.trim().length) {
-        // madeUp += currentChar;
         if (madeUp !== '') {
-          result.push(+madeUp);
+          result.push(madeUp as unknown as T);
           madeUp = '';
         }
       } else {
-        // console.log('SEE CHAR     : ', currentChar);
-        // console.log('No trim: ', currentChar.length);
-        // console.log('w/ trim: ', currentChar.trim().length);
-        // // console.log('SEE CHAR num1: ', +currentChar);
-        // // console.log('SEE CHAR num2: ', (currentChar as unknown as number).toString());
         madeUp += currentChar.trim();
       }
-      // console.log('Made up      : ', madeUp);
     }
 
     if (madeUp !== '') {
-      result.push(+madeUp);
+      result.push(madeUp as unknown as T);
     }
 
-    // console.log('SEE: ', result);
     return result;
   }
 }
